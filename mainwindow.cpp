fd37c59bd1854aaea6272c37ae0adb27f7e3a086
Tyulbashev Vladislav Sergeevich <vladislav.tyulbashev@yandex.ru>
2013-06-09 10:32:53 +0400

    My program downloaded first torrent

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,24 +1,47 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-#include <iostream>
-#include <fstream>
-#include <iterator>
-#include <iomanip>
-
-#include "libtorrent/torrent_info.hpp"
-#include "libtorrent/lazy_entry.hpp"
-#include <boost/filesystem/operations.hpp>
-
-
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    QObject::connect(ui->actionExit, SIGNAL(triggered()), this, SLOT(close()));
+    QObject::connect(ui->actionAdd_torrent, SIGNAL(triggered()), this, SLOT(addTorrent()));
+
+    initSession();
+    QTimer *timer = new QTimer;
+    timer->setInterval(1000);
+    QObject::connect(timer, SIGNAL(timeout()), this, SLOT(showInform()));
+    timer->start();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::initSession() {
+    session = new libtorrent::session;
+    session->listen_on(std::make_pair(6881, 6889));
+}
+
+void MainWindow::addTorrent() {
+    QString torrent = QFileDialog::getOpenFileName(this, QString(), QString(),
+                                                   QString("*.torrent"));
+    libtorrent::add_torrent_params p;
+    p.save_path = "/home/vlad/test/";
+    p.ti = new libtorrent::torrent_info(torrent.toStdString());
+    session->add_torrent(p);
+}
+
+void MainWindow::showInform() {
+    std::vector<libtorrent::torrent_handle> v = session->get_torrents();
+    qDebug() << v.size();
+    for (int i = 0; i < v.size(); i++) {
+        libtorrent::torrent_status s = v[i].status();
+        qDebug() << i << s.download_rate << s.total_download;
+    }
+    qDebug() << "------------------------------";
 }
--- mainwindow.h
+++ mainwindow.h
@@ -2,9 +2,24 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QFileDialog>
+#include <QTimer>
+#include <QDebug>
+
+#include <iostream>
+#include <fstream>
+#include <iterator>
+#include <iomanip>
+#include "libtorrent/entry.hpp"
+#include "libtorrent/bencode.hpp"
+#include "libtorrent/torrent_info.hpp"
+#include "libtorrent/lazy_entry.hpp"
+#include <libtorrent/session.hpp>
+#include <boost/filesystem/operations.hpp>
+
 
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 
 class MainWindow : public QMainWindow
@@ -17,6 +32,12 @@ public:
     
 private:
     Ui::MainWindow *ui;
+    libtorrent::session *session;
+
+    void initSession();
+private slots:
+    void addTorrent();
+    void showInform();
 };
 
 #endif // MAINWINDOW_H
